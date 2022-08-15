0fc4fefa18e9ba77a2544c89b8602fbe3fae2833
devpbdias <devpbdias@gmail.com>
2022-08-15 15:53:37 -0300

    feat: criando uma validação para nome do produto

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = require('./app');
+const validateName = require('./middlewares/validations');
 require('dotenv').config();
 
 app.use(express.json());
 
 const routes = require('./routes');
 
-app.use('/products', routes.productsRoute);
+app.use('/products', validateName, routes.productsRoute);
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
