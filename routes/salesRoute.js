6c787526f66a96101722c2610d41146a9383983a
DevPBDias <devpbdias@gmail.com>
2023-05-24 18:45:00 -0300

    feat: last commit before trybe-publisher

--- routes/salesRoute.js
+++ routes/salesRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const salesController = require('../controllers/salesController');
+const { validateIdQtty } = require('../middlewares/validations');
 
 const salesRoute = express.Router();
 
+salesRoute.post('/', validateIdQtty, salesController.addSales);
 salesRoute.get('/', salesController.getAllSales);
 salesRoute.get('/:id', salesController.getSalesById);
 
